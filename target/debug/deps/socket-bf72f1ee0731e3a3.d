5564ac21564a6f8b3b53088bc633498e454b7fee
Sukuna <1064687807@qq.com>
2024-11-28 16:46:16 +0800

    First commit

---
+++ target/debug/deps/socket-bf72f1ee0731e3a3.d
@@ -0,0 +1,5 @@
+/Users/xupengzhu/Desktop/learn-rust/rust-socket/target/debug/deps/socket-bf72f1ee0731e3a3: src/main.rs
+
+/Users/xupengzhu/Desktop/learn-rust/rust-socket/target/debug/deps/socket-bf72f1ee0731e3a3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/socket.d
@@ -0,0 +1,1 @@
+/Users/xupengzhu/Desktop/learn-rust/rust-socket/target/debug/socket: /Users/xupengzhu/Desktop/learn-rust/rust-socket/src/main.rs
